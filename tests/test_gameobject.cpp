536909d1acc3fc93ac1bb68af44e2b22bff8e074
mugurXD <mugurvlad47@gmail.com>
2026-09-13 23:03:13 +0300

    Add more unit tests for ECS

--- tests/test_gameobject.cpp
+++ tests/test_gameobject.cpp
@@ -22,6 +22,32 @@ namespace
 		int* startCount = nullptr;
 	};
 
+	struct Linked : lunar::Component_T {};
+
+	struct CountsUpdates : lunar::Component_T
+	{
+		void update(const lunar::FrameTime& frame_time) override
+		{
+			updates++;
+			lastDelta = frame_time.deltaTime;
+		}
+
+		int   updates   = 0;
+		float lastDelta = 0.f;
+	};
+
+	struct AlsoCountsUpdates : lunar::Component_T
+	{
+		void update(const lunar::FrameTime&) override
+		{
+			updates++;
+		}
+
+		int updates = 0;
+	};
+
+	constexpr float TEST_DELTA_TIME = 0.25f;
+
 	constexpr int   EXTRA_CAMERA_COUNT = 50;
 	constexpr float FLOAT_TOLERANCE    = 1e-5f;
 }
@@ -150,12 +176,12 @@ TEST(GameObject, ComponentBaseIsLinkedToItsObject)
 	lunar::GameObject object = scene.createGameObject("Object");
 	object->getTransform().position = { 4.f, 0.f, 0.f };
 
-	lunar::Camera* camera = object->addComponent<lunar::Camera>();
+	Linked* linked = object->addComponent<Linked>();
 
-	ASSERT_NE(camera, nullptr);
-	EXPECT_EQ(camera->getGameObject(), object);
-	EXPECT_EQ(camera->getScene(), &scene);
-	EXPECT_FLOAT_EQ(camera->getTransform().position.x, 4.f);
+	ASSERT_NE(linked, nullptr);
+	EXPECT_EQ(linked->getGameObject(), object);
+	EXPECT_EQ(linked->getScene(), &scene);
+	EXPECT_FLOAT_EQ(linked->getTransform().position.x, 4.f);
 }
 
 TEST(GameObject, AddingComponentCallsStartOnce)
@@ -173,13 +199,99 @@ TEST(GameObject, MainCameraSurvivesAddingMoreCameras)
 	lunar::Scene scene;
 
 	lunar::GameObject main_object = scene.createGameObject("Main");
-	scene.setMainCamera(main_object->addComponent<lunar::Camera>());
+	main_object->addComponent<lunar::Camera>()->fov = 90.f;
+	scene.setMainCamera(main_object);
 
 	for (int i = 0; i < EXTRA_CAMERA_COUNT; i++)
 		scene.createGameObject("Extra")->addComponent<lunar::Camera>();
 
 	ASSERT_NE(scene.getMainCamera(), nullptr);
-	EXPECT_EQ(scene.getMainCamera()->getGameObject(), main_object);
+	EXPECT_EQ(scene.getMainCamera(), main_object->getComponent<lunar::Camera>());
+	EXPECT_FLOAT_EQ(scene.getMainCamera()->fov, 90.f);
+}
+
+TEST(GameObject, MainCameraIsNullWithoutCamera)
+{
+	lunar::Scene scene;
+
+	EXPECT_EQ(scene.getMainCamera(), nullptr);
+
+	scene.setMainCamera(scene.createGameObject("No camera"));
+
+	EXPECT_EQ(scene.getMainCamera(), nullptr);
+}
+
+TEST(Behaviour, UpdateRunsOncePerFrameForEveryInstance)
+{
+	lunar::Scene scene;
+
+	lunar::GameObject first  = scene.createGameObject("First");
+	lunar::GameObject second = scene.createGameObject("Second");
+	first->addComponent<CountsUpdates>();
+	second->addComponent<CountsUpdates>();
+
+	scene.updateBehaviours(lunar::FrameTime{ .deltaTime = TEST_DELTA_TIME });
+	scene.updateBehaviours(lunar::FrameTime{ .deltaTime = TEST_DELTA_TIME });
+
+	EXPECT_EQ(first->getComponent<CountsUpdates>()->updates, 2);
+	EXPECT_EQ(second->getComponent<CountsUpdates>()->updates, 2);
+	EXPECT_FLOAT_EQ(first->getComponent<CountsUpdates>()->lastDelta, TEST_DELTA_TIME);
+}
+
+TEST(Behaviour, EveryBehaviourTypeIsUpdated)
+{
+	lunar::Scene scene;
+
+	lunar::GameObject object = scene.createGameObject("Object");
+	object->addComponent<CountsUpdates>();
+	object->addComponent<AlsoCountsUpdates>();
+
+	scene.updateBehaviours(lunar::FrameTime{});
+
+	EXPECT_EQ(object->getComponent<CountsUpdates>()->updates, 1);
+	EXPECT_EQ(object->getComponent<AlsoCountsUpdates>()->updates, 1);
+}
+
+TEST(Behaviour, DestroyedObjectsAreNotUpdated)
+{
+	lunar::Scene scene;
+
+	lunar::GameObject kept      = scene.createGameObject("Kept");
+	lunar::GameObject destroyed = scene.createGameObject("Destroyed");
+	kept->addComponent<CountsUpdates>();
+	destroyed->addComponent<CountsUpdates>();
+
+	destroyed->destroy();
+	scene.flushDestroyedEntities();
+	scene.updateBehaviours(lunar::FrameTime{});
+
+	EXPECT_EQ(kept->getComponent<CountsUpdates>()->updates, 1);
+}
+
+TEST(Camera, UpdateCamerasFollowsTransform)
+{
+	lunar::Scene scene;
+
+	lunar::GameObject parent = scene.createGameObject("Parent");
+	lunar::GameObject object = scene.createGameObject("Camera", parent);
+	parent->getTransform().position = { 0.f, 5.f, 0.f };
+	object->getTransform().position = { 1.f, 0.f, 0.f };
+	object->getTransform().rotation = { -90.f, 0.f, 0.f };
+	object->addComponent<lunar::Camera>();
+
+	lunar::UpdateCameras(scene, lunar::FrameTime{});
+
+	const lunar::Camera* camera = object->getComponent<lunar::Camera>();
+	EXPECT_NEAR(camera->front.x, 0.f, FLOAT_TOLERANCE);
+	EXPECT_NEAR(camera->front.z, -1.f, FLOAT_TOLERANCE);
+	EXPECT_NEAR(camera->right.x, 1.f, FLOAT_TOLERANCE);
+	EXPECT_FLOAT_EQ(camera->position.x, 1.f);
+	EXPECT_FLOAT_EQ(camera->position.y, 5.f);
+
+	const glm::vec4 origin_in_view = camera->getViewMatrix() * glm::vec4(camera->position, 1.f);
+	EXPECT_NEAR(origin_in_view.x, 0.f, FLOAT_TOLERANCE);
+	EXPECT_NEAR(origin_in_view.y, 0.f, FLOAT_TOLERANCE);
+	EXPECT_NEAR(origin_in_view.z, 0.f, FLOAT_TOLERANCE);
 }
 
 TEST(GameObject, WorldPositionUpdatesWhenParentMovesAfterQuery)
