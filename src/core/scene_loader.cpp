5ff107fedd0d9abc1937b4d99a507b055083fe21
mugurXD <mugurvlad47@gmail.com>
2026-09-16 23:56:13 +0300

    Comment out unused SceneLoader class (part 2)

--- src/core/scene_loader.cpp
+++ src/core/scene_loader.cpp
@@ -1,173 +1,173 @@
-#include <lunar/core/scene.hpp>
-#include <lunar/core/component.hpp>
-#include <lunar/render/components.hpp>
-#include <lunar/render/context.hpp>
-#include <lunar/render/mesh.hpp>
-#include <lunar/render/common.hpp>
-#include <lunar/file/json_file.hpp>
+//#include <lunar/core/scene.hpp>
+//#include <lunar/core/component.hpp>
+//#include <lunar/render/components.hpp>
+//#include <lunar/render/context.hpp>
+//#include <lunar/render/mesh.hpp>
+//#include <lunar/render/common.hpp>
+//#include <lunar/file/json_file.hpp>
 
-namespace lunar
-{
-	using namespace std;
+//namespace lunar
+//{
+//	using namespace std;
 
-	inline void LoadVec3f
-	(
-		const nlohmann::json& json,
-		const std::string_view& name,
-		glm::vec3& out
-	)
-	{
-		if (json.contains(name))
-		{
-			auto& data = json[name];
-			if (data.is_array())
-			{
-				out.x = data.size() > 0 ? data[0].get<float>() : out.x;
-				out.y = data.size() > 1 ? data[1].get<float>() : out.y;
-				out.z = data.size() > 2 ? data[2].get<float>() : out.z;;
-			}
-			else
-			{
-				out.x = data.value("x", out.x);
-				out.y = data.value("y", out.y);
-				out.z = data.value("z", out.z);
-			}
-		}
-	}
+//	inline void LoadVec3f
+//	(
+//		const nlohmann::json& json,
+//		const std::string_view& name,
+//		glm::vec3& out
+//	)
+//	{
+//		if (json.contains(name))
+//		{
+//			auto& data = json[name];
+//			if (data.is_array())
+//			{
+//				out.x = data.size() > 0 ? data[0].get<float>() : out.x;
+//				out.y = data.size() > 1 ? data[1].get<float>() : out.y;
+//				out.z = data.size() > 2 ? data[2].get<float>() : out.z;;
+//			}
+//			else
+//			{
+//				out.x = data.value("x", out.x);
+//				out.y = data.value("y", out.y);
+//				out.z = data.value("z", out.z);
+//			}
+//		}
+//	}
 
-	SceneLoader& SceneLoader::useRenderContext(Render::RenderContext context)
-	{
-		this->renderContext = context;
-		return *this;
-	}
+//	SceneLoader& SceneLoader::useRenderContext(Render::RenderContext context)
+//	{
+//		this->renderContext = context;
+//		return *this;
+//	}
 
-	SceneLoader& SceneLoader::useCoreSerializers()
-	{
-		useCustomClassSerializer("core.render.camera",        [](GameObject object, const nlohmann::json& json) {
-			Camera* camera = object.addComponent<Camera>();
-			camera->fov    = json.value<float>("fov", camera->fov);
+//	SceneLoader& SceneLoader::useCoreSerializers()
+//	{
+//		useCustomClassSerializer("core.render.camera",        [](GameObject object, const nlohmann::json& json) {
+//			Camera* camera = object.addComponent<Camera>();
+//			camera->fov    = json.value<float>("fov", camera->fov);
 
-			LoadVec3f(json, "front", camera->front);
-			LoadVec3f(json, "right", camera->right);
-			LoadVec3f(json, "up",    camera->up);
-		});
+//			LoadVec3f(json, "front", camera->front);
+//			LoadVec3f(json, "right", camera->right);
+//			LoadVec3f(json, "up",    camera->up);
+//		});
 
-		useCustomClassSerializer("core.render.mesh_renderer", [&](GameObject object, const nlohmann::json&) {
-			object.addComponent<MeshRenderer>();
+//		useCustomClassSerializer("core.render.mesh_renderer", [&](GameObject object, const nlohmann::json&) {
+//			object.addComponent<MeshRenderer>();
 
-			//if (json.contains("meshPath"))
-			//{
-			//	auto path = Fs::fromData(json["meshPath"]);
-			//	auto mesh_builder = Render::GpuMeshBuilder();
-			//	auto mesh = mesh_builder
-			//		.useRenderContext(renderContext.get())
-			//		.fromMeshFile(path)
-			//		.build();
+//			//if (json.contains("meshPath"))
+//			//{
+//			//	auto path = Fs::fromData(json["meshPath"]);
+//			//	auto mesh_builder = Render::GpuMeshBuilder();
+//			//	auto mesh = mesh_builder
+//			//		.useRenderContext(renderContext.get())
+//			//		.fromMeshFile(path)
+//			//		.build();
 
-			//	mesh_renderer->mesh = mesh;
-			//}
-		});
+//			//	mesh_renderer->mesh = mesh;
+//			//}
+//		});
 
 		
 
-		return *this;
-	}
+//		return *this;
+//	}
 
-	SceneLoader& SceneLoader::destination(Scene& scene)
-	{
-		this->result = &scene;
-		return *this;
-	}
+//	SceneLoader& SceneLoader::destination(Scene& scene)
+//	{
+//		this->result = &scene;
+//		return *this;
+//	}
 
-	SceneLoader& SceneLoader::useCustomClassSerializer(const std::string& name, const ComponentJsonParser& parser)
-	{
-		visitors[name] = parser;
-		return *this;
-	}
+//	SceneLoader& SceneLoader::useCustomClassSerializer(const std::string& name, const ComponentJsonParser& parser)
+//	{
+//		visitors[name] = parser;
+//		return *this;
+//	}
 
-	void SceneLoader::parseTransform
-	(
-		GameObject            object,
-		const nlohmann::json& json
-	)
-	{
-		auto& transform = object->getTransform();
-		if (json.contains("transform"))
-		{
-			auto& data = json["transform"];
-			LoadVec3f(data, "position", transform.position);
-			LoadVec3f(data, "rotation", transform.rotation);
-			LoadVec3f(data, "scale",    transform.scale);
-		}
-	}
+//	void SceneLoader::parseTransform
+//	(
+//		GameObject            object,
+//		const nlohmann::json& json
+//	)
+//	{
+//		auto& transform = object->getTransform();
+//		if (json.contains("transform"))
+//		{
+//			auto& data = json["transform"];
+//			LoadVec3f(data, "position", transform.position);
+//			LoadVec3f(data, "rotation", transform.rotation);
+//			LoadVec3f(data, "scale",    transform.scale);
+//		}
+//	}
 
-	inline void LoadComponent
-	(
-		GameObject                      object,
-		const SceneLoader::VisitorDict& visitors,
-		const nlohmann::json&           json
-	)
-	{
-		std::string type = json["type"];
-		if (not visitors.contains(type))
-		{
-			DEBUG_WARN("Found component of type '{}' but no visitor to parse it. Skipping...", type);
-			return;
-		}
+//	inline void LoadComponent
+//	(
+//		GameObject                      object,
+//		const SceneLoader::VisitorDict& visitors,
+//		const nlohmann::json&           json
+//	)
+//	{
+//		std::string type = json["type"];
+//		if (not visitors.contains(type))
+//		{
+//			DEBUG_WARN("Found component of type '{}' but no visitor to parse it. Skipping...", type);
+//			return;
+//		}
 
-		visitors.at(type)(object, json);
-	}
+//		visitors.at(type)(object, json);
+//	}
 
-	void SceneLoader::parseComponents
-	(
-		GameObject            object,
-		const nlohmann::json& json
-	)
-	{
-		if (json.contains("components"))
-		{
-			auto& data = json["components"];
-			for(auto& [ key, component ] : data.items())
-				LoadComponent(object, visitors, component);
-		}
-	}
+//	void SceneLoader::parseComponents
+//	(
+//		GameObject            object,
+//		const nlohmann::json& json
+//	)
+//	{
+//		if (json.contains("components"))
+//		{
+//			auto& data = json["components"];
+//			for(auto& [ key, component ] : data.items())
+//				LoadComponent(object, visitors, component);
+//		}
+//	}
 
-	void SceneLoader::parseGameObject
-	(
-		const nlohmann::json& json,
-		GameObject            parent
-	)
-	{
-		const std::string name = json.contains("name")
-			? json["name"]
-			: "GameObject";
+//	void SceneLoader::parseGameObject
+//	(
+//		const nlohmann::json& json,
+//		GameObject            parent
+//	)
+//	{
+//		const std::string name = json.contains("name")
+//			? json["name"]
+//			: "GameObject";
 
-		GameObject object = result->createGameObject(name, parent);
-		parseTransform(object, json);
-		parseComponents(object, json);
+//		GameObject object = result->createGameObject(name, parent);
+//		parseTransform(object, json);
+//		parseComponents(object, json);
 
-		if (json.contains("children"))
-		{
-			auto& children = json["children"];
-			for (auto& [key, child] : children.items())
-				parseGameObject(child, object);
-		}
-	}
+//		if (json.contains("children"))
+//		{
+//			auto& children = json["children"];
+//			for (auto& [key, child] : children.items())
+//				parseGameObject(child, object);
+//		}
+//	}
 
-	SceneLoader& SceneLoader::loadJsonFile(const Fs::Path& path)
-	{
-		auto  json_file = Fs::JsonFile(path);
-		auto& json      = json_file.content;
+//	SceneLoader& SceneLoader::loadJsonFile(const Fs::Path& path)
+//	{
+//		auto  json_file = Fs::JsonFile(path);
+//		auto& json      = json_file.content;
 		
-		result->setName(json.value<std::string>("name", "Unnamed Scene"));
+//		result->setName(json.value<std::string>("name", "Unnamed Scene"));
 		
-		if (json.contains("gameObjects"))
-		{
-			auto& data = json["gameObjects"];
-			for(auto& [ key, object ] : data.items())
-				parseGameObject(object, nullptr);
-		}
+//		if (json.contains("gameObjects"))
+//		{
+//			auto& data = json["gameObjects"];
+//			for(auto& [ key, object ] : data.items())
+//				parseGameObject(object, nullptr);
+//		}
 
-		return *this;
-	}
-}
+//		return *this;
+//	}
+//}
